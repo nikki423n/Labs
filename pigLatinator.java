83efc1129e4f30f4b381be744b3321f810214fe0
nicole Nguyen <nikki423n@gmail.com>
2019-10-04 01:50:31 -0700

    rough draft

--- pigLatinator.java
+++ pigLatinator.java
@@ -9,17 +9,25 @@ public class pigLatinator
         String englishWord = in.nextLine();
         String newWord = englishWord.replaceAll("[^a-zA-Z]", "");
         //removes all non-alpha characters like ? . , ; :
-        char[] arrayWord = newWord.toCharArray();
-        //converts string to an array of characters
         
-        String lowercase = englishWord.toLowerCase();
-        int[] englishWord = new Int[englishWord.length];
-        char vowels = {'a','e', 'i', 'o', 'u'};
-        
-        String noVowel = 0;
+        char[] vowels = {'a','e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        int vowelIndex = 100; //if no vowel, vowel index = 100
+
         boolean hasVowel = true;
+        int length = englishWord.length();
         
-        for (i = 0; i < englishWord.length; i++)
+        for (int i = 0; i < length; i++)
         {
-            if (lowercase != 
-        
+            if (hasVowel && (englishWord.charAt(0) != 'a' || englishWord.charAt(0) != 'e' || englishWord.charAt(0) != 'i' ||
+            englishWord.charAt(0) != 'o' || englishWord.charAt(0) != 'u'))
+                System.out.print( "ay");
+            else
+            if (hasVowel && (englishWord.charAt(0) == 'a' || englishWord.charAt(0) == 'e' || englishWord.charAt(0) == 'i' ||
+            englishWord.charAt(0) == 'o' || englishWord.charAt(0) == 'u'))
+            System.out.println(englishWord + "yay");
+            else
+            System.out.println(englishWord +"ay");
+            
+        }
+    }
+}
